570153e9673b2e6f61ad601989a5746b930e6642
Kai Gittens <kai.gittens@gmail.com>
2015-05-31 17:16:22 -0400

    staer gulp file

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require("gulp");
+var postcss = require('gulp-postcss');
+
+
+gulp.task('css', function () {
+  return gulp.src('src/**/*.css')
+    .pipe( postcss([
+      require('cssnext')(),
+      require('cssnano')()
+    ]) )
+    .pipe( gulp.dest('build/') );
+});
